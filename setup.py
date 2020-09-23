f8c0683d3c8f19a4979289a6b11635ac79f48191
mamadouyamar <71740811+mamadouyamar@users.noreply.github.com>
2020-09-22 22:33:47 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
     author_email="mamadou-yamar.thioub@hec.ca",
     description="Inference, goodness-of-fit tests, and predictions for continuous and discrete univariate Hidden Markov Models (HMM). The goodness-of-fit test is based on a Cramer-von Mises statistic and uses parametric bootstrap to estimate the p-value. The description of the methodology is taken from Nasri et al (2020) <doi: 10.1029/2019WR025122>.",
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/mamadouyamar/GenHMM1d",
     #packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -26,6 +26,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     packages=['GenHMM1d'],
-    install_requires=['scipy','numpy','joblib','math','multiprocessing'],
+    #install_requires=['scipy','numpy','joblib','math','multiprocessing'],
     python_requires='>=3.6',
 )
