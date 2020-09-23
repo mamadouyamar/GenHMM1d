1b1ed5aea3df22936544c40daaba0ae141e9d1f2
mamadouyamar <71740811+mamadouyamar@users.noreply.github.com>
2020-09-22 21:55:38 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -6,25 +6,9 @@ Created on Tue Sep 22 20:43:42 2020
 """
 import setuptools
 
-# setuptools.setup(
-#     # Needed to silence warnings (and to be a worthwhile package)
-#     name='Measurements',
-#     url='https://github.com/jladan/package_demo',
-#     author='John Ladan',
-#     author_email='jladan@uwaterloo.ca',
-    
-#     # Needed for dependencies
-#     install_requires=['scipy','numpy','functools','joblib','multiprocessing','math','scipy.optimize','scipy.stats'],
-    
-#     # *strongly* suggested for sharing
-#     version='0.1',
-    
-#     # The license can be anything you like
-#     license='MIT',
-#     description='An example of a python package from pre-existing code',
-#     # We will also need a readme eventually (there will be a warning)
-#     # long_description=open('README.txt').read(),
-# )
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 
 setuptools.setup(
